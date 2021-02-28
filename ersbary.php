ed4f8856a20830fc1d997c2b7ea7e437bb8aeab2
mmoha94 <m-mohammadi@tvu.ac.ir>
2021-02-28 09:49:38 +0330

    all file commited

---
+++ ersbary.php
@@ -0,0 +1,54 @@
+<?php
+class Person{
+    private $name;
+    private $family;
+    public function __construct($name,$family)
+    {
+        $this->name = $name;
+        $this->family = $family;
+    }
+    protected function ShowPerson()
+    {
+        echo "<br>NAME:".$this->name;
+        echo "<br>FAMILY:".$this->family;
+    }    
+}
+
+class Stud extends Person {
+    private $avgr;
+    public function __construct($name='',$family='',$avgr=0)
+    {
+        parent::__construct($name , $family);
+        $this->avgr = $avgr;
+    }
+    public function ShowInfo()
+    {
+        $this->ShowPerson();
+        echo "<br>AVERGAE:".$this->avgr;
+    }
+}
+class Karmand extends Person {
+    private $sallary;
+    public function __construct($name='',$family='',$sallary=0)
+    {
+        parent::__construct($name , $family);
+        $this->sallary = $sallary;
+    }
+    public function ShowInfo()
+    {
+        $this->ShowPerson();
+        echo "<br>SALLARY:".$this->sallary;
+    }
+}
+
+
+
+
+$s1 = new Stud('ali' , 'alavi' , 19.5 );
+$s1->ShowPerson();
+$s1->ShowInfo();
+
+$k1 = new Karmand('ali' , 'rezaee' ,500000);
+$k1->ShowInfo();
+
+?>
---
+++ stud.php
@@ -0,0 +1,35 @@
+<?php
+class Stud {
+    private $name;
+    private $family;
+    private $avgr;
+    public function __construct($name='',$family='',$avgr=0)
+    {
+        $this->name = $name;
+        $this->family = $family;
+        $this->avgr = $avgr;
+    }
+    public function setInfo($name,$family,$avgr)
+    {
+        $this->name = $name;
+        $this->family = $family;
+        $this->avgr = $avgr;
+    }
+
+    public function ShowInfo()
+    {
+        echo "<br>NAME:".$this->name;
+        echo "<br>FAMILY:".$this->family;
+        echo "<br>AVERGAE:".$this->avgr;
+    }
+}
+
+
+
+$s1 = new Stud('ali','alavi',18);
+$s1->ShowInfo();
+
+$s2 = new Stud();
+$s2->ShowInfo();
+
+?>
